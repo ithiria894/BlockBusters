23920b97b19b2a7af5af422fb5945bd37f7985b4
jakejakeho <mtho@connect.ust.hk>
2023-04-04 23:12:38 +0800

    support get all nft

--- pages/api/v1/nft.js
+++ pages/api/v1/nft.js
@@ -1,6 +1,28 @@
 import clientPromise from "../../../utils/mongoClient";
 
 export default async (req, res) => {
+  if (req.method === 'GET') {
+    get(req, res)
+  }
+  if (req.method === 'POST') {
+    post(req, res)
+  }
+};
+
+const get = async(req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("nft");
+    const nft = await db.collection("nft").find({}).toArray();
+    
+    res.json(nft);
+  } catch (e) {
+    console.error(e);
+    throw new Error(e).message;
+  }
+}
+
+const post = async(req, res) => {
   try {
     const client = await clientPromise;
     const db = client.db("nft");
@@ -18,4 +40,4 @@ export default async (req, res) => {
     console.error(e);
     throw new Error(e).message;
   }
-};
+}
